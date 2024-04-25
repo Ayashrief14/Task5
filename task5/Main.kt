a71504079f8b17901ff1ca2df9fdc3a833fae386
AyaSharief <118765195+Ayashrief14@users.noreply.github.com>
2024-04-25 21:06:40 +0200

    Update Main.kt

--- task5/Main.kt
+++ task5/Main.kt
@@ -1,17 +1,23 @@
 fun main() {
-    val importantTask1 = ImportantTask("cleaning the room", "should be done today", "Aya")
+    println("user name please : ")
+    val importantTask1 = ImportantTask("cleaning the room", "should be done today", readln())
+    println("what is the name of the task ?")
+    importantTask1.title = readln()
+    println(" what is the description of the task ?")
+    importantTask1.description = readln()
+    println("what is the priority of this task ? (number )")
+    val taskPriority = readln().toInt()
+    println("did you completed the task (true or false ?")
+    val taskCompleted = readln().toBoolean()
+
+
+
     println( importantTask1.user )
     println("-" .repeat(15))
     println( importantTask1.title  )
     println( importantTask1.description)
     println("-" .repeat(15))
-    importantTask1.priority = 4
+    importantTask1.priority = taskPriority
     println("-" .repeat(15))
-    importantTask1.completed = true
 
-
-
-
-
-
-}
+    importantTask1.completed = taskCompleted
