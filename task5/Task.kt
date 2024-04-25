fa768e5bbce6d4ffd57c4add78666aed55351217
AyaSharief <118765195+Ayashrief14@users.noreply.github.com>
2024-04-25 20:37:52 +0200

    Update Task.kt

--- task5/Task.kt
+++ task5/Task.kt
@@ -2,8 +2,13 @@ import kotlin.properties.Delegates
 
  open class Task(title : String, description : String,  user: String, ) {
     var completed : Boolean by Delegates.observable(false){property, oldValue, newValue ->
-        println("Task completed")
-        println("True")
+        if (newValue){
+            println("Task completed")
+            println("True")
+        }else{
+            println("Task uncompleted")
+            println("false")
+        }
     }
       val user : String by lazy {"user is $user \n $user \n $user " }
 
